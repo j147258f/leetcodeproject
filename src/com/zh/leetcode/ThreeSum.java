399ea898df46eef4a01bc7676b45f624c6532969
zhh@ekuaibao.com <zhh@ekuaibao.com>
2018-09-04 19:32:22 +0800

    15 三个数的和为0。未完成 采用，以空间换时间的算法，进行中

--- src/com/zh/leetcode/ThreeSum.java
+++ src/com/zh/leetcode/ThreeSum.java
@@ -1,51 +1,78 @@
 package com.zh.leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 //没忍住又做了一题，实现真的low，算法超时
 public class ThreeSum {
     public static void main(String[] strings) {
-        ThreeSum s=new ThreeSum();
-        s.threeSum(new int[]{0, 0, 0, 0});
+        ThreeSum s = new ThreeSum();
+        s.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 
-    List<List<Integer>> fatherList = new LinkedList<>();
 
     public List<List<Integer>> threeSum(int[] nums) {
-        int index1 = 0;
-        int index2 = 1;
+        List<Data> fatherList = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 int lastNum = nums[i] + nums[j];
                 for (int k = j + 1; k < nums.length; k++) {
                     if (nums[k] == -lastNum) {
-                        List<Integer> sonList = new LinkedList<>();
-                        sonList.add(nums[i]);
-                        sonList.add(nums[j]);
-                        sonList.add(nums[k]);
-                        if (!isInFatherList(sonList)) fatherList.add(sonList);
+                        Data d = new Data();
+                        d.produceList(nums[i], nums[j], nums[k]);
+                        if (!isInFatherList(d, fatherList)) fatherList.add(d);
                     }
                 }
             }
         }
-        return fatherList;
+        return fatherList.stream().map(e -> e.list).collect(Collectors.toList());
     }
 
-    private boolean isInFatherList(List<Integer> sonList) {
-        for (List<Integer> item : fatherList) {
-            List<Integer> itemCopy=item.stream().collect(Collectors.toList());
-            int index1 = itemCopy.indexOf(sonList.get(0));
-            if(index1>-1)itemCopy.remove(index1);
-            int index2 = itemCopy.indexOf(sonList.get(1));
-            if(index2>-1)itemCopy.remove(index2);
-            int index3 = itemCopy.indexOf(sonList.get(2));
-            if(index3>-1)itemCopy.remove(index3);
-            boolean allHas=index1>-1&&index2>-1&&index3>-1;
-            if(allHas)return true;
+    private boolean isInFatherList(Data d, List<Data> fatherList) {
+        for (Data item : fatherList) {
+            if (item.onlineNum == d.onlineNum) {
+                if(item.hasZero==d.hasZero){
+                    return true;
+                }{
+                    return false;
+                }
+            }
         }
         return false;
+    }
+
+    static class Data {
+        private List<Integer> list = new ArrayList<>(3);
+        int onlineNum;
+        boolean hasZero = false;
+
+        public List<Integer> produceList(int... ints) {
+            int bigNum = 0;
+            int index = 0;
+            int bigIndex = 0;
+            int littleIndex = 0;
+            for (int i : ints) {
+                list.add(i);
+                if (i > 0) {
+                    bigNum++;
+                    bigIndex = index;
+                } else if (i == 0) {
+                    hasZero = true;
+                } else {
+                    littleIndex = index;
+                }
+
+                index++;
+            }
+            if (bigNum == 1) {
+                onlineNum = ints[bigIndex];
+            } else if (bigNum == 0) {
+                onlineNum = 0;
+            } else {
+                onlineNum = ints[littleIndex];
+            }
+            return list;
+        }
     }
 }
