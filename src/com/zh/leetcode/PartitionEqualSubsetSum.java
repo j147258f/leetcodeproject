db97f396ee9504191b231dbc8374a530025ec030
zhanghao <j147258f@163.com>
2019-04-18 04:52:19 +0800

    动态规划

--- src/com/zh/leetcode/PartitionEqualSubsetSum.java
+++ src/com/zh/leetcode/PartitionEqualSubsetSum.java
@@ -4,10 +4,13 @@ import java.math.BigDecimal;
 
 public class PartitionEqualSubsetSum {
     public static void main(String[] strs) {
-        FindMax();
+//        FindMax();
+        canPartition(new int[]{1, 2, 3, 4, 5, 6, 7});
+//        canPartition(new int[]{1,5,11,5});
     }
 
-    public boolean canPartition(int[] nums) {
+    //由于全是正整数，所以保证和要小于half；此时每加一个数会逼近half
+    public static boolean canPartition(int[] nums) {
         int sum = 0;
         for (int num : nums) {
             sum += num;
@@ -19,31 +22,48 @@ public class PartitionEqualSubsetSum {
         int half = sum / 2;
 
         //找出数组中某几个数的合是否 == half
-        int[][] result = new int[nums.length][half];
-
+        int[][] result = new int[nums.length + 1][half + 1];
+        int[] numsClone = new int[nums.length + 1];
+        for (int i = 1; i < numsClone.length; i++) {
+            numsClone[i] = nums[i - 1];
+        }
+        //i代表方到第几个数
         for (int i = 1; i <= nums.length; i++) {
+            //j代表当前目标和
             for (int j = 1; j <= half; j++) {
-                if (j < nums[i]) {
+                if (j < numsClone[i]) {
                     //数字放不进去
-                    result[i - 1][j] = result[i][j];
+                    result[i][j] = result[i - 1][j];
                 } else {
-//                    if(result[i-1][j] > result[i-1][j-nums[i]]+)
+                    //能放进去的时候，判定放和不放哪个的和距离目标数近
+//                    int notPut = j - result[i - 1][j];
+//                    int put = j - (result[i - 1][j] + numsClone[i]);
+
+                    int notPut = j - result[i - 1][j];
+                    int put = j - (result[i - 1][j - numsClone[i]] + numsClone[i]);
+                    if (put >= 0 && notPut > put) {
+                        result[i][j] = result[i - 1][j - numsClone[i]] + numsClone[i];
+                    } else {
+                        //放距离目标数最近
+                        result[i][j] = result[i - 1][j];
+                    }
                 }
 
             }
         }
-
-
+        if (result[nums.length][half] == half) {
+            return true;
+        }
         return false;
     }
 
     static void FindMax()//动态规划
     {
-        int number=7;
-        int capacity=8;
-        int[] w={0,1,2,8,4,5,6,7};
-        int[] v={0,1,100,113,4,5,80,7};
-        int[][] V = new int[number+1][capacity+1];
+        int number = 7;
+        int capacity = 8;
+        int[] w = {0, 1, 2, 8, 4, 5, 6, 7};
+        int[] v = {0, 1, 100, 113, 4, 5, 80, 7};
+        int[][] V = new int[number + 1][capacity + 1];
         int i, j;
         //填表
         for (i = 1; i <= number; i++) {
@@ -63,7 +83,7 @@ public class PartitionEqualSubsetSum {
                 }
             }
         }
-        int sssd=0;
+        int sssd = 0;
     }
 
 }
