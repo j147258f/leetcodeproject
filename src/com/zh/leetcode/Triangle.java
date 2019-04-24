82ce0d8423a105f5a55e88820cc5ba6484b961c7
zhanghao <j147258f@163.com>
2019-04-24 10:56:37 +0800

    三角形最小路径和

---
+++ src/com/zh/leetcode/Triangle.java
@@ -0,0 +1,21 @@
+package com.zh.leetcode;
+
+import java.util.List;
+
+/**
+ * 三角形最小路径和
+ */
+public class Triangle {
+/*
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+ */
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        return 0;
+    }
+}
