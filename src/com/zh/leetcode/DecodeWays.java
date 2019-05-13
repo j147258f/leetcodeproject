318c2395b48f65009e8cbd120d833164063205b0
zhanghao <j147258f@163.com>
2019-05-14 00:25:59 +0800

    解码方法（草稿）

---
+++ src/com/zh/leetcode/DecodeWays.java
@@ -0,0 +1,12 @@
+package com.zh.leetcode;
+
+public class DecodeWays {
+    public static void main(String[] strs){
+        System.out.println((int)'A');
+    }
+
+    public int numDecodings(String s) {
+
+        return 0;
+    }
+}
