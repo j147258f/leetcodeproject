7cf992b3096682e994aebeafb3574ce271791bb8
zhanghao <j147258f@163.com>
2020-10-20 16:00:29 +0800

    温故知新？之前写的递归好烂

--- src/com/zh/leetcode/UniquePaths.java
+++ src/com/zh/leetcode/UniquePaths.java
@@ -1,14 +1,14 @@
 package com.zh.leetcode;
 
 import java.util.LinkedList;
-import java.util.Stack;
 
 /**
  * Created by zhanghao on 2019/3/28.
  */
 public class UniquePaths {
     public static void main(String[] strings) {
-        System.out.println(uniquePaths(25, 12));
+        System.out.println(solu(51, 9));
+        System.out.println(uniquePaths1(51, 9));
 //        System.out.println(uniquePaths1(99, 99));
     }
 
@@ -20,29 +20,41 @@ public class UniquePaths {
 
     public static int uniquePaths1(int m, int n) {
         int[][] range = new int[m][n];
-        for(int i =0;i<n;i++){
-            range[0][i]=1;
+        for (int i = 0; i < n; i++) {
+            range[0][i] = 1;
         }
-        for (int i=0;i<m;i++){
-            range[i][0]=1;
+        for (int i = 0; i < m; i++) {
+            range[i][0] = 1;
         }
-        for(int i =1;i<m;i++){
-            for(int j =1;j<n;j++){
-                range[i][j]=range[i-1][j]+range[i][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                range[i][j] = range[i - 1][j] + range[i][j - 1];
             }
         }
-        return range[m-1][n-1];
+        return range[m - 1][n - 1];
+    }
+
+
+    public static int solu(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (m == 1) return  1;
+        if (n == 1) return  1;
+
+        return solu(m - 1, n) + solu(m, n - 1);
     }
 
     /**
      * 复杂度太高
+     *
      * @param rightLength 横坐标
      * @param downLength  纵坐标
      * @return
      */
     public static int uniquePaths(int rightLength, int downLength) {
         if ((rightLength == 2 && downLength == 1) || (rightLength == 1 && downLength == 2)) return 1;
-        if(rightLength==1&&downLength==1)return 1;
+        if (rightLength == 1 && downLength == 1) return 1;
         popedWay = null;
         path = new LinkedList<>();
         remanentDown = downLength - 1;
