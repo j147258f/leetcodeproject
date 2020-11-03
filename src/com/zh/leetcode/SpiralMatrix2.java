56c4538c040e228dfd77ef772c715e844a8da35f
zhanghao <j147258f@163.com>
2020-11-03 19:04:25 +0800

    和螺旋矩阵1没啥区别

---
+++ src/com/zh/leetcode/SpiralMatrix2.java
@@ -0,0 +1,79 @@
+package com.zh.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpiralMatrix2 {
+    public int[][] generateMatrix(int n) {
+        result = new int[n][n];
+        if (n == 0) return result;
+        top = 0;
+        left = 0;
+        down = result.length - 1;
+        right = result[0].length - 1;
+        while (top < down || left < right) {
+            if (!(right(left, right) && down(top, down) && left(right, left) && up(down, top))) {
+                break;
+            }
+
+        }
+        if (top == down && left == right) {
+            result[top][left] = count;
+            count++;
+        }
+        return result;
+
+    }
+
+    int count = 1;
+    int[][] result;
+    int top;
+    int left;
+    int down;
+    int right;
+
+    public boolean right(int start, int end) {
+        boolean f = false;
+        for (int i = start; i <= end; i++) {
+            f = true;
+            result[top][i] = count;
+            count++;
+        }
+        top++;
+
+        return f;
+    }
+
+    public boolean down(int start, int end) {
+        boolean f = false;
+        for (int i = start; i <= end; i++) {
+            f = true;
+            result[i][right] = count;
+            count++;
+        }
+        right--;
+        return f;
+    }
+
+    public boolean left(int start, int end) {
+        boolean f = false;
+        for (int i = start; i >= end; i--) {
+            f = true;
+            result[down][i] = count;
+            count++;
+        }
+        down--;
+        return f;
+    }
+
+    public boolean up(int start, int end) {
+        boolean f = false;
+        for (int i = start; i >= end; i--) {
+            f = true;
+            result[i][left] = count;
+            count++;
+        }
+        left++;
+        return f;
+    }
+}
