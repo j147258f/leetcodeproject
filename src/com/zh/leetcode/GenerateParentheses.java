c17dec90ce140b6c944692bca2ba4fdaa1f4d837
zhanghao <j147258f@163.com>
2020-11-13 18:34:29 +0800

    暂时跳过

---
+++ src/com/zh/leetcode/GenerateParentheses.java
@@ -0,0 +1,15 @@
+package com.zh.leetcode;
+
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * 没思路 先做别的
+ */
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+
+}
