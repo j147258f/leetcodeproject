81b97b3ea80f52a2734750f799fea95189390b7b
zhanghao <j147258f@163.com>
2021-01-26 20:37:35 +0800

    掷骰子 动态规划解法

--- src/com/zh/leetcode/foroffer/o60.java
+++ src/com/zh/leetcode/foroffer/o60.java
@@ -1,9 +1,30 @@
 package com.zh.leetcode.foroffer;
 
 public class o60 {
+    public static void main(String[] args){
+        new o60().dicesProbability(2);
+    }
+
     //动态规划
     public double[] dicesProbability(int n) {
-        return null;
+        int[][] dp = new int[n][6 * n];
+        for (int i = 1; i <= 6; i++) {
+            dp[0][i - 1] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = i ; j < (i+1 ) * 6; j++) {
+                for (int h = 1; j - h>=0&&h<=6; h++) {
+                    dp[i][j] += dp[i - 1][j - h];
+                }
+            }
+        }
+        double[] count = new double[6 * n - n + 1];
+        double count_sum = Math.pow(6d, (double) n);
+        for (int i = 0; i < count.length; i++) {
+            count[i] = dp[n-1][i+n-1] / count_sum;
+        }
+
+        return count;
     }
 
 
